fb09eefd44d4ddabcf5f70828bcd66d9a9005bdd
biasimatheushenrique40 <biasimatheushenrique40@gmail.com>
2024-01-03 05:20:34 +0000

    Adicionando uoload de arquivo csv

--- src/app/componentes/importacao/importacao.component.ts
+++ src/app/componentes/importacao/importacao.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+
 
 @Component({
-  selector: 'app-importacao',
+  selector: 'my-importacao',
   templateUrl: './importacao.component.html',
   styleUrls: ['./importacao.component.css']
 })
-export class ImportacaoComponent {
+export class ImportacaoComponent implements OnInit {
+
+  title = 'IMPORTAÇÃO DE ARQUIVOS';
+  dados = '';
+
+  constructor() {}
+
+  ngOnInit() {
+  }
+
+  onFileChange(event: Event) {
+    const target = event.target as HTMLInputElement;
+    const file: File = (target.files as FileList)[0];
+
+    const reader: FileReader = new FileReader();
+
+    reader.onloadend = (e) => {
+      const fileContent: string = reader.result as string;
+      console.log(fileContent);
+      this.dados = fileContent;
+
+    };
+
+    reader.readAsText(file);
+  }
+  
 
 }
