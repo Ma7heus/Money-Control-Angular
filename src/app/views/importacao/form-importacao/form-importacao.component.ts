68f285fd84df93d7a260d8435be8964ad83d1bd8
MATHEUS HENRIQUE SALDIAS PALACIOS PIETRO BIAS <86833526+Ma7heus@users.noreply.github.com>
2024-06-30 20:17:09 -0300

    Criando Form Importação de Arquivo

---
+++ src/app/views/importacao/form-importacao/form-importacao.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-importacao',
+  standalone: true,
+  imports: [],
+  templateUrl: './form-importacao.component.html',
+  styleUrl: './form-importacao.component.css'
+})
+export class FormImportacaoComponent {
+
+}
