315d6ac8cfadfd9470a838208bab6ee9ad216895
MATHEUS HENRIQUE SALDIAS PALACIOS PIETRO BIAS <86833526+Ma7heus@users.noreply.github.com>
2024-07-16 23:02:40 -0300

    Criando serviço de alerta

---
+++ src/app/core/common/services/alert.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AlertService {
+
+  constructor(private _snackBar: MatSnackBar) { }
+
+  openSnackBar(message: string, panelClass: string) {
+    this._snackBar.open(message, 'Fechar', {
+      duration: 3000,
+      horizontalPosition: 'center',
+      verticalPosition: 'top',
+      panelClass: panelClass
+    });
+  }
+
+  showSuccess(message: string) {
+    this.openSnackBar('✅ ' + message, 'success-snackbar');
+  }
+
+  showWarning(message: string) {
+    this.openSnackBar('⚠️ ' + message, 'warning-snackbar');
+  }
+
+  showError(message: string) {
+    this.openSnackBar('❌ ' + message, 'error-snackbar');
+  }
+}
